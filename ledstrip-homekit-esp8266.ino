d583b6b85d739a1eefd9692189e9a5051e4683cd
Nicolas Rey <reynico@gmail.com>
2024-02-13 12:00:11 -0300

    add a rotary encoder to tune brightness

--- ledstrip-homekit-esp8266.ino
+++ ledstrip-homekit-esp8266.ino
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <arduino_homekit_server.h>
 #include "wifi_info.h"
+#include "AiEsp32RotaryEncoder.h"
 
 #define LOG_D(fmt, ...) printf_P(PSTR(fmt "\n"), ##__VA_ARGS__);
 
@@ -8,15 +9,29 @@ bool received_sat = false;
 bool received_hue = false;
 bool received_brightness = false;
 
-int r_pin = 13; // Wemos - D7
-int g_pin = 12; // Wemos - D6
-int b_pin = 14; // Wemos - D5
+// RGB Led Strip outputs
+int r_pin = 13;  // Wemos - D7
+int g_pin = 12;  // Wemos - D6
+int b_pin = 14;  // Wemos - D5
 
+// Rotary encoder inputs
+int clk_pin = 2;  // Wemos - D4
+int dt_pin = 0;   // Wemos - D3
+int sw_pin = 4;   // Wemos - D2
+int steps = 8;
+
+// Local states
 bool is_on = false;
 float current_brightness = 100.0;
 float current_sat = 0.0;
 float current_hue = 0.0;
 int rgb_colors[3];
+
+AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(clk_pin, dt_pin, sw_pin, -1, steps);
+
+void IRAM_ATTR readEncoderISR() {
+  rotaryEncoder.readEncoder_ISR();
+}
 
 void setup() {
   Serial.begin(115200);
@@ -26,14 +41,38 @@ void setup() {
   pinMode(g_pin, OUTPUT);
   pinMode(b_pin, OUTPUT);
 
-  delay(1000);
+  pinMode(clk_pin, INPUT);
+  pinMode(dt_pin, INPUT);
+  pinMode(sw_pin, INPUT);
+
+  rotaryEncoder.begin();
+  rotaryEncoder.setup(readEncoderISR);
+  rotaryEncoder.setBoundaries(0, 100, false);
+  rotaryEncoder.disableAcceleration();
+  rotaryEncoder.setEncoderValue(current_brightness);
+
   rgb_colors[0] = 255;
   rgb_colors[1] = 255;
   rgb_colors[2] = 255;
+  analogWrite(r_pin, 255);
+  analogWrite(g_pin, 255);
+  analogWrite(b_pin, 255);
   my_homekit_setup();
 }
 
+void rotary_loop() {
+  if (rotaryEncoder.encoderChanged()) {
+    current_brightness = rotaryEncoder.readEncoder();
+    received_brightness = true;
+    homekit_value_t v;
+    v.int_value = current_brightness;
+    set_bright(v);
+  }
+  // handle_rotary_button();
+}
+
 void loop() {
+  rotary_loop();
   my_homekit_loop();
   delay(10);
 }
@@ -60,6 +99,10 @@ void my_homekit_setup() {
   cha_hue.setter = set_hue;
 
   arduino_homekit_setup(&accessory_config);
+
+  // report the switch value to HomeKit if it is changed (e.g. by a physical button)
+  cha_bright.value.int_value = current_brightness;
+  homekit_characteristic_notify(&cha_bright, cha_bright.value);
 }
 
 void my_homekit_loop() {
@@ -75,7 +118,7 @@ void my_homekit_loop() {
 
 void set_on(const homekit_value_t v) {
   bool on = v.bool_value;
-  cha_on.value.bool_value = on;  //sync the value
+  cha_on.value.bool_value = on;  // sync the value
 
   if (on) {
     is_on = true;
@@ -116,6 +159,8 @@ void set_bright(const homekit_value_t v) {
   LOG_D("Set brightness to %d", bright);
 
   current_brightness = bright;
+  rotaryEncoder.setEncoderValue(current_brightness);
+  homekit_characteristic_notify(&cha_bright, cha_bright.value);  // sync the value
   received_brightness = true;
 
   updateColor();
@@ -123,7 +168,6 @@ void set_bright(const homekit_value_t v) {
 
 void updateColor() {
   if (is_on) {
-
     if (received_hue && received_sat || received_brightness) {
       HSV2RGB(current_hue, current_sat, current_brightness);
       received_hue = false;
@@ -135,8 +179,7 @@ void updateColor() {
     analogWrite(r_pin, 255 - rgb_colors[0]);
     analogWrite(g_pin, 255 - rgb_colors[1]);
     analogWrite(b_pin, 255 - rgb_colors[2]);
-  } else if (!is_on)  // lamp - switch to off
-  {
+  } else if (!is_on) {  // lamp - switch to off
     LOG_D("Lamp is powered off");
     analogWrite(r_pin, 255 - 0);
     analogWrite(g_pin, 255 - 0);
@@ -145,7 +188,6 @@ void updateColor() {
 }
 
 void HSV2RGB(float h, float s, float v) {
-
   int i;
   float m, n, f;
 
@@ -169,7 +211,6 @@ void HSV2RGB(float h, float s, float v) {
   n = v * (1 - s * f);
 
   switch (i) {
-
     case 0:
     case 6:
       rgb_colors[0] = round(v * 255);
