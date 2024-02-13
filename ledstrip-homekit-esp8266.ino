60524208c89241134fd01b84193448f4dec540dc
Nicolas Rey <reynico@gmail.com>
2024-02-13 19:14:04 -0300

    add extra behaviour to the rotary encoder

--- ledstrip-homekit-esp8266.ino
+++ ledstrip-homekit-esp8266.ino
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 #include <arduino_homekit_server.h>
 #include "wifi_info.h"
-#include "AiEsp32RotaryEncoder.h"
+#include "lib/AiEsp32RotaryEncoder.h"
+#include "lib/AiEsp32RotaryEncoder.cpp"
 
 #define LOG_D(fmt, ...) printf_P(PSTR(fmt "\n"), ##__VA_ARGS__);
 
@@ -26,6 +27,12 @@ float current_brightness = 100.0;
 float current_sat = 0.0;
 float current_hue = 0.0;
 int rgb_colors[3];
+unsigned long button_short_press_ms = 200;
+unsigned long button_long_press_ms = 500;
+
+const int BRIGHTNESS_MODE = 0;
+const int COLOR_MODE = 1;
+int rotary_mode = BRIGHTNESS_MODE;
 
 AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(clk_pin, dt_pin, sw_pin, -1, steps);
 
@@ -57,18 +64,73 @@ void setup() {
   analogWrite(r_pin, 255);
   analogWrite(g_pin, 255);
   analogWrite(b_pin, 255);
+
+  randomSeed(analogRead(0));
   my_homekit_setup();
+}
+
+
+void on_button_short_click() {
+  if (rotary_mode == BRIGHTNESS_MODE) {
+    rotary_mode = COLOR_MODE;
+  } else {
+    rotary_mode = BRIGHTNESS_MODE;
+  }
+  LOG_D("Rotary mode changed to %s mode", (rotary_mode == BRIGHTNESS_MODE) ? "brightness" : "color");
+}
+
+void on_button_long_click() {
+  homekit_value_t v;
+  v.bool_value = !is_on;
+  set_on(v);
+}
+
+void handle_rotary_button() {
+  static unsigned long last_time_button_down = 0;
+  static bool was_button_down = false;
+
+  bool is_encoder_buton_down = rotaryEncoder.isEncoderButtonDown();
+
+  if (is_encoder_buton_down) {
+    if (!was_button_down) {
+      // start measuring
+      last_time_button_down = millis();
+    }
+    // else we wait since button is still down
+    was_button_down = true;
+    return;
+  }
+
+  // button is up
+  if (was_button_down) {
+    // click happened, lets see if it was short click, long click or just too short
+    if (millis() - last_time_button_down >= button_long_press_ms) {
+      on_button_long_click();
+    } else if (millis() - last_time_button_down >= button_short_press_ms) {
+      on_button_short_click();
+    }
+  }
+  was_button_down = false;
 }
 
 void rotary_loop() {
   if (rotaryEncoder.encoderChanged()) {
-    current_brightness = rotaryEncoder.readEncoder();
-    received_brightness = true;
-    homekit_value_t v;
-    v.int_value = current_brightness;
-    set_bright(v);
+    if (rotary_mode == BRIGHTNESS_MODE) {
+      current_brightness = rotaryEncoder.readEncoder();
+      received_brightness = true;
+      homekit_value_t v;
+      v.int_value = current_brightness;
+      set_bright(v);
+    } else if (rotary_mode == COLOR_MODE) {
+      current_hue = random(1.0, 230.0);
+      current_sat = random(1.0, 100.0);
+      received_hue = true;
+      received_sat = true;
+
+      updateColor();
+    }
   }
-  // handle_rotary_button();
+  handle_rotary_button();
 }
 
 void loop() {
@@ -103,17 +165,19 @@ void my_homekit_setup() {
   // report the switch value to HomeKit if it is changed (e.g. by a physical button)
   cha_bright.value.int_value = current_brightness;
   homekit_characteristic_notify(&cha_bright, cha_bright.value);
+  cha_on.value.bool_value = is_on;
+  homekit_characteristic_notify(&cha_on, cha_on.value);
 }
 
 void my_homekit_loop() {
   arduino_homekit_loop();
-  const uint32_t t = millis();
-  if (t > next_heap_millis) {
-    // show heap info every 5 seconds
-    next_heap_millis = t + 5 * 1000;
-    LOG_D("Free heap: %d, HomeKit clients: %d",
-          ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
-  }
+  // const uint32_t t = millis();
+  // if (t > next_heap_millis) {
+  //   // show heap info every 5 seconds
+  //   next_heap_millis = t + 5 * 1000;
+  //   LOG_D("Free heap: %d, HomeKit clients: %d",
+  //         ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
+  // }
 }
 
 void set_on(const homekit_value_t v) {
@@ -127,6 +191,8 @@ void set_on(const homekit_value_t v) {
     is_on = false;
     LOG_D("Lamp is off");
   }
+
+  homekit_characteristic_notify(&cha_on, cha_on.value);
 
   updateColor();
 }
@@ -159,8 +225,10 @@ void set_bright(const homekit_value_t v) {
   LOG_D("Set brightness to %d", bright);
 
   current_brightness = bright;
-  rotaryEncoder.setEncoderValue(current_brightness);
-  homekit_characteristic_notify(&cha_bright, cha_bright.value);  // sync the value
+  // keep the encoder value
+  if (rotary_mode == BRIGHTNESS_MODE) {
+    rotaryEncoder.setEncoderValue(current_brightness);
+  }
   received_brightness = true;
 
   updateColor();
@@ -173,6 +241,16 @@ void updateColor() {
       received_hue = false;
       received_sat = false;
       received_brightness = false;
+
+      // When the color is set from the rotary encoder,
+      // Also notify HomeKit about the chosen color.
+      cha_hue.value.float_value = current_hue;
+      homekit_characteristic_notify(&cha_hue, cha_hue.value);
+      cha_sat.value.float_value = current_sat;
+      homekit_characteristic_notify(&cha_sat, cha_sat.value);
+      cha_bright.value.int_value = current_brightness;
+      homekit_characteristic_notify(&cha_bright, cha_bright.value);
+      LOG_D("current_hue %.1f, current_sat %.1f, current_brightness %.1f", current_hue, current_sat, current_brightness);
     }
 
     LOG_D("Update color to rgb(%d, %d, %d)", rgb_colors[0], rgb_colors[1], rgb_colors[2]);
